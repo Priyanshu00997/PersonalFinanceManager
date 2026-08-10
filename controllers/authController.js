8af04171fb53ed31cef4cd548c4e2be7377a05a7
Priyanshu Gupta <priyanshu00997@gmail.com>
2026-08-10 21:59:37 +0530

    add

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,7 @@
 const db = require("../config/db");
 
 exports.showRegister = (req, res) => {
-
     res.render("register");
-
 };
 
 exports.showLogin = (req, res) => {
@@ -11,7 +9,6 @@ exports.showLogin = (req, res) => {
 };
 
 exports.registerUser = (req, res) => {
-
     const { fullname, email, password } = req.body;
 
     const sql = `
@@ -20,12 +17,48 @@ exports.registerUser = (req, res) => {
     `;
 
     db.query(sql, [fullname, email, password], (err) => {
-
         if (err) {
             console.log(err);
             return res.send("Registration Failed");
         }
 
         res.redirect("/login");
+    });
+};
+exports.loginUser = (req, res) => {
+
+    console.log("LOGIN FUNCTION CALLED");
+
+    const { email, password } = req.body;
+
+    console.log("Email:", email);
+    console.log("Password:", password);
+
+    const sql = `
+        SELECT * FROM users
+        WHERE email = ?
+    `;
+
+    db.query(sql, [email], (err, results) => {
+
+        if (err) {
+            console.log("Database Error:", err);
+            return res.send("Database Error");
+        }
+
+        console.log("Results:", results);
+
+        if (results.length === 0) {
+            return res.send("Invalid email or password");
+        }
+
+        const user = results[0];
+
+        if (user.password !== password) {
+            return res.send("Invalid email or password");
+        }
+
+        res.send("Login Successful ✅");
+        res.redirect("/dashboard");
     });
 };
